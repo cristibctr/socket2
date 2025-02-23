97fd44c7e38cc23fa555b657898a7fcd70bc1c3d
Cristian <cristian.bucataru@gmail.com>
2025-02-23 18:54:52 +0200

    Revert "Suffix Socket::(set_)recv_tos with v4"
    This reverts commit 171d1eb57a9b71956971fbcb0949a6b8dee55e2f.

--- tests/socket.rs
+++ tests/socket.rs
@@ -1437,7 +1437,7 @@ test!(IPv4 tos_v4, set_tos_v4(96));
     target_os = "vita",
     target_os = "haiku",
 )))]
-test!(IPv4 recv_tos_v4, set_recv_tos_v4(true));
+test!(IPv4 recv_tos, set_recv_tos(true));
 
 #[cfg(not(windows))] // TODO: returns `WSAENOPROTOOPT` (10042) on Windows.
 test!(IPv4 broadcast, set_broadcast(true));
