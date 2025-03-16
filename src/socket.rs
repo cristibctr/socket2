a9e60394e491084a28859a603ef54cd7b4dc5f18
Cristian <cristian.bucataru@gmail.com>
2025-03-16 17:07:36 +0200

    Revert "Remove Socket::(set_)header_included (#547)"
    This reverts commit 25337f182c3902250548c04f8d22c38d41a9d796.

--- src/socket.rs
+++ src/socket.rs
@@ -1129,17 +1129,42 @@ const fn into_linger(duration: Option<Duration>) -> sys::linger {
 /// * Linux: <https://man7.org/linux/man-pages/man7/ip.7.html>
 /// * Windows: <https://docs.microsoft.com/en-us/windows/win32/winsock/ipproto-ip-socket-options>
 impl Socket {
+    /// This method is deprecated, use [`crate::Socket::header_included_v4`].
+    #[cfg(all(feature = "all", not(any(target_os = "redox", target_os = "espidf"))))]
+    #[cfg_attr(
+        docsrs,
+        doc(cfg(all(feature = "all", not(any(target_os = "redox", target_os = "espidf")))))
+    )]
+    #[deprecated = "Use `Socket::header_included_v4` instead"]
+    pub fn header_included(&self) -> io::Result<bool> {
+        self.header_included_v4()
+    }
     /// Get the value of the `IP_HDRINCL` option on this socket.
     ///
-    /// For more information about this option, see [`set_header_included_v4`].
+    /// For more information about this option, see [`set_header_included`].
     ///
-    /// [`set_header_included_v4`]: Socket::set_header_included_v4
+    /// [`set_header_included`]: Socket::set_header_included
     #[cfg(all(feature = "all", not(any(target_os = "redox", target_os = "espidf"))))]
     pub fn header_included_v4(&self) -> io::Result<bool> {
         unsafe {
             getsockopt::<c_int>(self.as_raw(), sys::IPPROTO_IP, IP_HDRINCL)
                 .map(|included| included != 0)
         }
+    }
+
+    /// This method is deprecated, use [`crate::Socket::set_header_included_v4`].
+    #[cfg_attr(
+        any(target_os = "fuchsia", target_os = "illumos", target_os = "solaris"),
+        allow(rustdoc::broken_intra_doc_links)
+    )]
+    #[cfg(all(feature = "all", not(any(target_os = "redox", target_os = "espidf"))))]
+    #[cfg_attr(
+        docsrs,
+        doc(cfg(all(feature = "all", not(any(target_os = "redox", target_os = "espidf")))))
+    )]
+    #[deprecated = "Use `Socket::set_header_included_v4` instead"]
+    pub fn set_header_included(&self, included: bool) -> io::Result<()> {
+        self.set_header_included_v4(included)
     }
 
     /// Set the value of the `IP_HDRINCL` option on this socket.
@@ -1632,9 +1657,9 @@ impl Socket {
 impl Socket {
     /// Get the value of the `IP_HDRINCL` option on this socket.
     ///
-    /// For more information about this option, see [`set_header_included_v6`].
+    /// For more information about this option, see [`set_header_included`].
     ///
-    /// [`set_header_included_v6`]: Socket::set_header_included_v6
+    /// [`set_header_included`]: Socket::set_header_included
     #[cfg(all(
         feature = "all",
         not(any(
